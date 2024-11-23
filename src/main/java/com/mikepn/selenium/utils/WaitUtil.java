7210afe9205c302906a590cd6f348f4002908f78
mikepn02 <nzaberamikepeter@gmail.com>
2024-11-23 19:32:46 +0200

    updated: keyboardtest

---
+++ src/main/java/com/mikepn/selenium/utils/WaitUtil.java
@@ -0,0 +1,28 @@
+package com.mikepn.selenium.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtil extends Util {
+
+    public static void explicitWait(int seconds , By locator){
+        WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+
+    public static void FluentWait(int seconds , By locator){
+        FluentWait fluentWait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(seconds))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class, StaleElementReferenceException.class);
+
+        fluentWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
---
+++ src/test/java/com/mikepn/selenium/pages/interactions/KeyboardTest.java
@@ -0,0 +1,2 @@
+package com.mikepn.selenium.pages.interactions;public class KeyboardTest {
+}
